bad72dfef8381afab34d91e35302ba5fe6a1f10e
priyanshi123456 <priyanshij251@gmail.com>
2025-10-31 19:22:53 +0530

    Fixed lint errors and added alt tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <img src='menu.jpg'></img>
+      <h2>🍕 Welcome to Our Pizza Menu!</h2>
+      <img src="menu.jpg" alt="Pizza Menu Page 1" style={{ width: '90%', maxWidth: '400px' }} />
+     
     </div>
   );
 }
